1bc8da4e3b37fc09933a481f240b31dfc15da662
ferransorde <ferran.sorde01@estudiant.upf.edu>
2024-04-29 22:53:17 +0200

    Sheeps

---
+++ Assets/RW/Scripts/DemolishOnTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemolishOnTrigger : MonoBehaviour
+{
+    public string tagFilter;
+
+    private void OnTriggerEnter (Collider other) 
+    {
+        if (other.CompareTag(tagFilter)) 
+        {
+            Destroy(gameObject); 
+        }
+    }
+}
